2812ffecea4389531c480af137e6efadff89b57b
Sophie Kowalski <skowalski57@gmail.com>
2020-04-21 17:04:00 +0200

    second part of the ravenous exercice

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,7 +11,6 @@ class SearchBar extends React.Component {
   renderSortByOptions() {
     return Object.keys(sortByOptions).map((sortByOption) => {
       let sortByOptionValue = sortByOptions[sortByOption];
-
       return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
   }
@@ -20,14 +19,14 @@ class SearchBar extends React.Component {
     return (
       <div className="SearchBar">
         <div className="SearchBar-sort-options">
-          <ul>{this.renderSortByOptions}</ul>
+          <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="SearchBar-fields">
           <input placeholder="Search Businesses" />
           <input placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-          <a>Let's Go</a>
+          <a href="/">Let's Go</a>
         </div>
       </div>
     );
